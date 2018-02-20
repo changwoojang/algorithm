d839f34b1aa34a27bea4edb352c0c9ba8b616a1c
Changwoo Jang <changwooj111@gmail.com>
2018-02-20 11:51:53 +0900

    Add : Codility 테스트 코드 추가

--- src/PhoneBook/PhoneBookAppHashMap.java
+++ src/PhoneBook/PhoneBookAppHashMap.java
@@ -11,21 +11,21 @@ public class PhoneBookAppHashMap {
     final int EXIT = -1;
 
     private Scanner read;
-    private HashMap<String,Phone> map = new HashMap<String, Phone>();
+    private HashMap<String, Phone> map = new HashMap<String, Phone>();
 
     private String information;
-    private String phoneNum,name;
+    private String phoneNum, name;
 
-    public PhoneBookAppHashMap(){
+    public PhoneBookAppHashMap() {
         read = new Scanner(System.in);
     }
 
-    public void add(){
+    public void add() {
 
         System.out.print("ADD");
         System.out.print("How many phone numbers?");
         int n = read.nextInt();
-        for(int i = 0 ; i < n ; i ++) {
+        for (int i = 0; i < n; i++) {
             System.out.print("Name : " + ", PhoneNum : ");
             information = read.next();
             String[] eachInformation = information.split(",");
@@ -35,46 +35,57 @@ public class PhoneBookAppHashMap {
         }
     }
 
-    public void delete(){
+    public void delete() {
         System.out.println("DELETE");
         System.out.print("Name : ");
         name = read.next();
-        if(map.get(name) == null){
+        if (map.get(name) == null) {
             System.out.print("Can't find");
-            return ;
+            return;
         }
         map.remove(name);
     }
 
-    public void search(){
+    public void search() {
         System.out.println("SEARCH");
         System.out.print("Name : ");
         name = read.next();
         System.out.println(map.get(name));
     }
-    public void showAll(){
-        for(String each : map.keySet()){
-            System.out.println("EACH : "+ map.get(each));
+
+    public void showAll() {
+        for (String each : map.keySet()) {
+            System.out.println("EACH : " + map.get(each));
         }
     }
 
-    public void run(){
+    public void run() {
         int[] test = new int[6];
-        int tests [] = new int[6];
+        int tests[] = new int[6];
 
-        while(true){
+        while (true) {
             System.out.println("Create:1, Search:2, Delete:3, All:4, Exit:-1");
             int menu = read.nextInt();
-            switch (menu){
-                case CREATE: add(); break;
-                case DELETE: delete(); break;
-                case SEARCH: search(); break;
-                case ALL: showAll(); break;
-                case EXIT: break;
+            switch (menu) {
+                case CREATE:
+                    add();
+                    break;
+                case DELETE:
+                    delete();
+                    break;
+                case SEARCH:
+                    search();
+                    break;
+                case ALL:
+                    showAll();
+                    break;
+                case EXIT:
+                    break;
             }
         }
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         PhoneBookAppHashMap pba = new PhoneBookAppHashMap();
 
         pba.run();
